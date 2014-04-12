5cc95e51f72ce9a722302f01315cc4ff1f2eb1ae
sjs7007 <sjs7007@gmail.com>
2014-04-13 02:13:52 +0530

    <Test cases added.>

---
+++ CFG/test/Integers2.c
@@ -0,0 +1,16 @@
+//Integer level 2 : Internal variables generated.
+
+#include<stdio.h>
+void main()
+{
+	int i,j,k=0;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<=3;j++)
+		{
+			k = 5;
+			k = 7*k + 55 *43 -34 +23 + 3*4 + 32;
+			printf("%d %d",i,j);
+		}
+	}
+}

544c88f866b1f0f1d3f0519152ad4ce2557a0ced
sjs7007 <sjs7007@gmail.com>
2014-06-08 08:31:12 +0530

    <theory on how to improved added>

--- CFG/test/MatrixMul1.c
+++ CFG/test/MatrixMul1.c
@@ -11,23 +11,23 @@ int main()
   scanf("%d%d", &m, &n);
   printf("Enter the elements of first matrix\n");
  
-  for (  c = 0 ; c < m ; c++ )
+  /*for (  c = 0 ; c < m ; c++ )
     for ( d = 0 ; d < n ; d++ )
       scanf("%d", &first[c][d]);
- 
+ */
   printf("Enter the number of rows and columns of second matrix\n");
   scanf("%d%d", &p, &q);
  
-  if ( n != p )
+  /*if ( n != p )
     printf("Matrices with entered orders can't be multiplied with each other.\n");
   else
-  {
+  {*/
     printf("Enter the elements of second matrix\n");
  
-    for ( c = 0 ; c < p ; c++ )
+    /*for ( c = 0 ; c < p ; c++ )
       for ( d = 0 ; d < q ; d++ )
         scanf("%d", &second[c][d]);
- 
+ */
     for ( c = 0 ; c < m ; c++ )
     {
       for ( d = 0 ; d < q ; d++ )
@@ -42,7 +42,7 @@ int main()
       }
     }
  
-    printf("Product of entered matrices:-\n");
+   /* printf("Product of entered matrices:-\n");
  
     for ( c = 0 ; c < m ; c++ )
     {
@@ -51,7 +51,7 @@ int main()
  
       printf("\n");
     }
-  }
+  }*/
  
   return 0;
 }
